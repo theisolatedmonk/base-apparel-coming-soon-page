e5e4d0352081be4ab5f17b1f26f4e43105179617
theisolatedmonk <96rahulseth@gmail.com>
2023-06-21 12:29:05 +0530

    font-imported

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,10 +7,10 @@ import arrow from '@/images/icon-arrow.svg'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-4">
+    <main className="flex min-h-screen flex-col items-center justify-between bg-white font-JosefinSans">
      
-<div className="flex">
-  <Image src={logo} alt={''}/>
+<div className="flex  items-start justify-start w-full p-8">
+  <Image src={logo} alt={''} className=''/>
 
 </div>
 
@@ -21,9 +21,10 @@ export default function Home() {
 </div>
 
 <p className="">Hello fellow shoppers! We're currently building our new fashion store. Add your email below to stay up-to-date with announcements and our launch deals.</p>
-<div className=" flex">
-  <input type="email"  className=' rounded-lg outline-none'/>
-  <Image src={arrow} alt={''}/>
+<div className=" flex border-2 rounded-full">
+  <input type="email" placeholder='Email Address' className='  px-4 outline-none text-black
+   rounded-full'/>
+<div className="bg-[hsl(0,93%,68%)] px-6 py-2 rounded-full">  <Image src={arrow} alt={''} className=''/></div>
 
 </div>
     </main>
